c3a8842b1190f498078fbd7a879bf29ab9abb457
Antoine <20175226+antooinex@users.noreply.github.com>
2023-11-06 15:02:13 -0500

    bugfix

--- log320_hnefatafl/src/log320_hnefatafl/StrategieAleatoire.java
+++ log320_hnefatafl/src/log320_hnefatafl/StrategieAleatoire.java
@@ -15,15 +15,17 @@ public class StrategieAleatoire implements Strategie {
     		for(int yDep = 1; yDep <= 13; yDep += 1) {
     			for(int xArr = 1; xArr <= 13; xArr += 1) {
     				for(int yArr = 1; yArr <= 13; yArr +=1) {
-						sb.append(board.antiCoord(xDep));
-						sb.append(Integer.toString(yDep));
-						sb.append(" - ");
-						sb.append(board.antiCoord(xArr));
-						sb.append(Integer.toString(yArr));
-						if(board.update(sb.toString(), equipe, false)){
-							possibilites.add(sb.toString());
-						}
-						sb.setLength(0);
+    					if(xDep == xArr || yDep == yArr) {
+							sb.append(board.antiCoord(xDep));
+							sb.append(Integer.toString(yDep));
+							sb.append(" - ");
+							sb.append(board.antiCoord(xArr));
+							sb.append(Integer.toString(yArr));
+							if(board.update(sb.toString(), equipe, false)){
+								possibilites.add(sb.toString());
+							}
+							sb.setLength(0);
+    					}
     				}
     			}
     		}
